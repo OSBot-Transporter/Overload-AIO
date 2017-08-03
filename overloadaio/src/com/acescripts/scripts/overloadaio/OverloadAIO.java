3bc8b34ed9157dc8820cd00074373a4c8d122279
Transporter <shayblyth@live.co.uk>
2017-08-03 18:01:30 +0100

    Update OverloadAIO.java

--- overloadaio/src/com/acescripts/scripts/overloadaio/OverloadAIO.java
+++ overloadaio/src/com/acescripts/scripts/overloadaio/OverloadAIO.java
@@ -618,8 +618,8 @@ public class OverloadAIO extends Script implements MessageListener {
     }
 
     private boolean sendSignatureData(long runtimeInSeconds, int var1, int var2, int var3, int var4, int var5) {
-        String privateKey = "199 26 173 152 8";
-        String initVector = "199 26 173 152 8";
+        String privateKey = "";
+        String initVector = "";
 
         try {
             String data = initVector+","+getClient().getUsername()+","+runtimeInSeconds+","+var1+","+var2+","+var3+","+var4+","+var5;
